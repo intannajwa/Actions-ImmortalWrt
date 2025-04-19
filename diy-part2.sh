c5d75fa8c309fe3bb1733f7bf8948f7cd5898a1b
Intan Najwa binti Ahmad <126443940+intannajwa@users.noreply.github.com>
2025-04-20 02:07:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
 
 # Replace ash with bash
 sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
 
 # Partition alignment
-sed -i 's/256/4096/g' target/linux/x86/image/Makefile
+#sed -i 's/256/4096/g' target/linux/x86/image/Makefile
