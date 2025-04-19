7a80b231d045209e20b19b6be476ace4bef328f9
Intan Najwa binti Ahmad <126443940+intannajwa@users.noreply.github.com>
2025-04-19 12:08:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,9 +10,6 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
-
 # Replace ash with bash
 sed -i 's/\/bin\/ash/\/bin\/bash/' package/base-files/files/etc/passwd
 
